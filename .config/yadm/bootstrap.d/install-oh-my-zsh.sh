b1da7c4ff82c5d82add54a69028ee1ca524568a7
Matteo Danelon <md@umh.app>
2022-11-14 11:15:58 +0100

    yadm: bootstrap oh-my-zsh

---
+++ .config/yadm/bootstrap.d/install-oh-my-zsh.sh
@@ -0,0 +1,11 @@
+#  _____ ______
+# |  __ \___  /	Matteo 'DaruZero' Danelon
+# | |  | | / / 	
+# | |  | |/ /  	https://matteodanelon.com
+# | |__| / /__ 	https://github.com/DaruZero
+# |_____/_____|
+#
+# Install oh-my-zsh
+
+! [[ -d $HOME/.oh-my-zsh ]] && sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
