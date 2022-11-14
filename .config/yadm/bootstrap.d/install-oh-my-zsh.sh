66fbbb4db428668c90e8a6b15c682747ecf30566
Matteo Danelon <92927530+DaruZero@users.noreply.github.com>
2022-11-14 11:49:55 +0100

    yadm: fix oh-my-zsh bootstrap

--- .config/yadm/bootstrap.d/install-oh-my-zsh.sh
+++ .config/yadm/bootstrap.d/install-oh-my-zsh.sh
@@ -7,5 +7,6 @@
 #
 # Install oh-my-zsh
 
-! [[ -d $HOME/.oh-my-zsh ]] && sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
+if ! [[ -d $HOME/.oh-my-zsh ]];
+        then sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
