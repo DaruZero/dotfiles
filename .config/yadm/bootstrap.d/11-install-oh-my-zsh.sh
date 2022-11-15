d510a7cc5fb9c36271f8d2a140859c2a609f8210
Matteo Danelon <md@umh.app>
2022-11-15 16:30:29 +0100

    fix(yadm): oh-my-zsh bootstrap script

--- .config/yadm/bootstrap.d/11-install-oh-my-zsh.sh
+++ .config/yadm/bootstrap.d/11-install-oh-my-zsh.sh
@@ -8,7 +8,8 @@
 #
 # Install oh-my-zsh
 
-if ! [[ -d $HOME/.oh-my-zsh ]]; then
+# if not existing execute install script and restore original .zshrc
+if [ ! -d $HOME/.oh-my-zsh ]; then
     sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
     command mv $HOME/.zshrc.pre-oh-my-zsh $HOME.zshrc
 fi
